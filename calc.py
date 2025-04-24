780d38964c6d30276c2fc6fb48fc55bc284631ef
Kahekiiii <dejesus.309062@laspinas.sti.edu.ph>
2025-04-24 12:40:43 +0800

    Update calc.py
    Functions: {BinOp() and Num()} {...}

--- calc.py
+++ calc.py
@@ -98,8 +98,22 @@ class Lexer(object):
             self.error()
             
         return Token(EOF, None)
-        
-class Interpreter(object):
+
+class AST(object):
+    pass
+
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+class Parser(object):
     def __init__(self, lexer):
         self.lexer = lexer
         # set current token to the first token taken from the input
@@ -126,27 +140,23 @@ class Interpreter(object):
         token = self.current_token
         if token.type == INTEGER:
             self.eat(INTEGER)
-            return token.value
+            return Num(token)  # Fix: return Num instead of token.value
         elif token.type == LPAREN:
             self.eat(LPAREN)
-            result = self.expr()
+            node = self.expr()
             self.eat(RPAREN)
-            return result
+            return node
         
     def term(self):
         """term : factor ((MUL | DIV) factor)*"""
-        result = self.factor()
+        node = self.factor()
         
         while self.current_token.type in (MUL, DIV):
             token = self.current_token
-            if token.type == MUL:
-                self.eat(MUL)
-                result = result * self.factor()
-            elif token.type == DIV:
-                self.eat(DIV)
-                result = result / self.factor()
-                
-        return result
+            self.eat(token.type)
+            node = BinOp(left=node, op=token, right=self.factor())
+        
+        return node
         
     def expr(self):
         """Arithmetic expression parser / interpreter.
@@ -155,36 +165,70 @@ class Interpreter(object):
         
         expr    : term ((PLUS | MINUS) term)*
         term    : factor ((MUL | DIV) factor)*
-        factor  : INTEGER | LPAREN expr RPARN
+        factor  : INTEGER | LPAREN expr RPAREN
         """
         
-        result = self.term()
+        node = self.term()
         
         while self.current_token.type in (PLUS, MINUS):
             token = self.current_token
-            if token.type == PLUS:
-                self.eat(PLUS)
-                result = result + self.term()
-            elif token.type == MINUS:
-                self.eat(MINUS)
-                result = result - self.term()
+            self.eat(token.type)
+            node = BinOp(left=node, op=token, right=self.term())
         
-        return result
-        
+        return node
+
+    def parse(self):
+        return self.expr()
+
+class NodeVisitor(object):
+    def visit(self, node):
+        method_name = 'visit_' + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)  # Fix: correct typo in 'self.generic_visit'
+        return visitor(node)
+
+    def generic_visit(self, node):
+        raise Exception('No visit_{} method'.format(type(node).__name__))
+
+class Interpreter(NodeVisitor):
+    def __init__(self, parser):
+        self.parser = parser
+
+    def visit_BinOp(self, node):
+        if node.op.type == PLUS:
+            return self.visit(node.left) + self.visit(node.right)
+        elif node.op.type == MINUS:
+            return self.visit(node.left) - self.visit(node.right)
+        elif node.op.type == MUL:
+            return self.visit(node.left) * self.visit(node.right)
+        elif node.op.type == DIV:
+            return self.visit(node.left) / self.visit(node.right)
+    
+    def visit_Num(self, node):
+        return node.value
+
+    def interpret(self):
+        tree = self.parser.parse()  # Fix: correct typo in 'self.parser'
+        return self.visit(tree)
+    
 def main():
     
     while True:
         try:
             # To run under Python3 replace 'raw_input' call
             # with 'input'
-            text = input('calc> ')
+            try:
+                text = raw_input('spi> ')
+            except NameError: # Python3
+                text = input('spi> ')
         except EOFError:
             break
         if not text:
             continue
+            
         lexer = Lexer(text)
-        interpreter = Interpreter(lexer)
-        result = interpreter.expr()
+        parser = Parser(lexer)
+        interpreter = Interpreter(parser)
+        result = interpreter.interpret()
         print(result)
         
 if __name__ == '__main__':
